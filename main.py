8576b9197b739a7cf760f0f36cbbd2ca6165b157
Ibrahim <ibrahimmuhammad018@gmail.com>
2022-06-13 12:01:00 +0000

    Finding Anagrams Task
     My first commit for Anagrams Task with Zuri

---
+++ main.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    if sorted(word) == sorted(anagram):
+        return True
+    else:
+        return False
+
+
+print(find_anagram("elbow", "below"))
+print(find_anagram("listen", "silent"))
+print(find_anagram("weflof", "fellow"))
+print(find_anagram("mcarch", "charm"))
